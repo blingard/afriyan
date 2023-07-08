3599fbfabe8ce876e1e77b905b27b4c9ace8fce5
Belinga Jean PAul <jeanpaulellabelinga@gmail.com>
2023-07-08 17:12:55 +0100

    Initial commit

---
+++ src/test/java/org/ligot/afriyan/OngafriyanApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ligot.afriyan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OngafriyanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
